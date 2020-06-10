8e36707560bea966f3a5eec8d5b12b1d7e334be0
Nhan Doan <nhandev88@gmail.com>
2020-06-11 04:07:14 +0700

    Update new font

--- scratch/src/utils/typography.js
+++ scratch/src/utils/typography.js
@@ -4,10 +4,10 @@ export default new Typography({
   baseFontSize: 20,
   baseLineHeight: 1.6,
   googleFonts: [
-    { name: 'Noto Sans', styles: ['700'] },
+    { name: 'Nunito', styles: ['700'] },
     { name: 'Lora', styles: ['400', '400i', '700', '700i'] },
     { name: 'IBM Plex Mono', styles: ['400'] },
   ],
-  headerFontFamily: ['Noto Sans', 'sans-serif'],
+  headerFontFamily: ['Nunito', 'sans-serif'],
   bodyFontFamily: ['Lora', 'serif'],
 });
