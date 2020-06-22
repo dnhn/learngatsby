7292f958ffb6b518a32c0eaad9655f1ee042165c
Nhan Doan <nhandev88@gmail.com>
2020-06-23 01:18:32 +0700

    Create Netlify headers by plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,6 +28,7 @@ module.exports = {
     'gatsby-plugin-sharp',
     'gatsby-transformer-sharp',
     'gatsby-plugin-sitemap',
+    'gatsby-plugin-netlify',
     {
       resolve: `gatsby-source-filesystem`,
       options: {
