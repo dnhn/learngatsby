7c1b4c91943ab1c3a91269c64e77e8d40f8c18e5
Nhan Doan <nhandev88@gmail.com>
2020-06-25 04:50:43 +0700

    Update lazy loading

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,19 +1,19 @@
 import React, { Suspense, lazy } from 'react';
+import SEO from '../components/seo';
 import { isSSR } from '../utils/constant';
 
-const SEO = lazy(() => import('../components/seo'));
 const Nav = lazy(() => import('../components/nav'));
 const Autocomplete = lazy(() => import('../components/autocomplete'));
 
 export default ({ children, location }) => (
   <main>
+    <SEO />
     {!isSSR && (
       <Suspense fallback={`Loading…`}>
-        <SEO />
         <Nav />
         <Autocomplete location={location} />
-        {children}
       </Suspense>
     )}
+    {children}
   </main>
 );
