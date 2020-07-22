46ac2e523ab5c311ab23a403bbc9397824c4515c
Nhan Doan <nhandev88@gmail.com>
2020-07-22 17:14:48 +0700

    Update social meta tags

--- src/templates/q.jsx
+++ src/templates/q.jsx
@@ -20,11 +20,33 @@ export default ({
     <Meta property="og:type" content="article" />
     <Meta property="og:title" content={post.frontmatter.title} />
     <Meta property="og:description" content={post.excerpt} />
-    <Meta property="og:image" content="https://unsplash.it/640/320" />
+    <Meta property="og:image" content="https://unsplash.it/480/240" />
     <Meta name="twitter:card" content="summary_large_image" />
     <Meta name="twitter:title" content={post.frontmatter.title} />
     <Meta name="twitter:descrition" content={post.excerpt} />
-    <Meta name="twitter:image" content="https://unsplash.it/640/320" />
+    <Meta name="twitter:image" content="https://unsplash.it/480/240" />
+
+    {post.frontmatter.poster && (
+      <>
+        {post.frontmatter.poster.external &&
+          <>
+            <Meta property="og:image" content={post.frontmatter.poster.external} />
+            <Meta name="twitter:image" content={post.frontmatter.poster.external} />
+          </>}
+        {post.frontmatter.poster.local &&
+          <>
+            <Meta
+              property="og:image"
+              content={`${siteMetadata.siteUrl}${post.frontmatter.poster.local.publicURL}`}
+            />
+            <Meta
+              name="twitter:image"
+              content={`${siteMetadata.siteUrl}${post.frontmatter.poster.local.publicURL}`}
+            />
+          </>}
+      </>
+    )}
+
     <HeadLink
       rel="canonical"
       href={`${siteMetadata.siteUrl}${post.fields.slug}`}
@@ -77,10 +99,10 @@ export default ({
     <h4>{post.timeToRead} phút</h4>
     {post.frontmatter.poster && (
       <>
-        {post.frontmatter.poster.local &&
-          <img src={post.frontmatter.poster.local.publicURL} alt="" />}
         {post.frontmatter.poster.external &&
           <img src={post.frontmatter.poster.external} alt="" />}
+        {post.frontmatter.poster.local &&
+          <img src={post.frontmatter.poster.local.publicURL} alt="" />}
       </>
     )}
     <div dangerouslySetInnerHTML={{ __html: post.html }} />
--- src/utils/constant.js
+++ src/utils/constant.js
@@ -2,4 +2,4 @@ export const googleFonts = 'https://fonts.googleapis.com/css?family=' +
   'Nunito:700|Lora:400,400i,700,700i|IBM+Plex+Mono:400' +
   '&display=swap';
 
-export const CDN = '';
+export const CDN = 'https://res.cloudinary.com';
